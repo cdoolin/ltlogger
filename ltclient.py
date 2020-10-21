a6432a13a8910c9881330b6e350760203b30e8ee
Callum Doolin <callum@resolvedinstruments.com>
2020-10-21 16:28:29 -0600

    ltclient works with python 3

--- ltclient.py
+++ ltclient.py
@@ -11,12 +11,14 @@ class LTClient(object):
         self.s.connect("tcp://%s:%d" % (server, int(port)))
 
     def config(self, name, label="", units=""):
-        self.s.send("config;%s;%s;%s" % (name, label, units))
+        msg = "config;%s;%s;%s" % (name, label, units)
+        self.s.send(msg.encode())
         self.s.recv()
 
     def put(self, name, value, time=None):
         if time is None:
             time = now()
 
-        self.s.send("put;%s;%s;%s" % (name, value, time))
+        msg = "put;%s;%s;%s" % (name, value, time)
+        self.s.send(msg.encode())
         self.s.recv()
